1410b873a517f69ed4e6f75bdcad507e6a276928
ssamueliuss <samuelg1234sadam@gmail.com>
2025-05-01 23:04:06 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,12 @@ document.querySelectorAll('button').forEach(button => {
         audio.play();
     });
 });
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// Deshabilitar teclas como F12, Ctrl+Shift+I, Ctrl+U
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'F12' || (e.ctrlKey && (e.key === 'u' || e.key === 'U' || e.shiftKey && e.key === 'I'))) {
+        e.preventDefault();
+    }
+});
